bc54b0589f8d5884b1451167c7e9b6fd5f0f882c
lubowen <bowem.lu@eascs.com>
2020-05-25 15:43:43 +0800

    初始化项目

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <container></container>
+</template>
+
+<script>
+    import container from '@/components/container'
+
+    export default {
+        name: 'Home',
+        components: {
+            container
+        }
+    }
+</script>
+
+<style>
+    
+</style>
